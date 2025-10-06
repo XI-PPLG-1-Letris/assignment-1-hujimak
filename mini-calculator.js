f0594ca6243ff8cbfbf8b1950457aa42f76e4a8d
darlius/feature <darliushia123@gmail.com>
2025-10-06 19:37:45 +0700

    mini-calculator

---
+++ mini-calculator.js
@@ -0,0 +1,27 @@
+a = 20
+b = 5
+
+// PENJUMLAHAN
+const tambah = a + b
+console.log("Hasil Penjumlahan 20 + 5 =", tambah);
+// Output yang akan keluar adalah 25
+
+// PENGURANGAN
+const kurang = a - b
+console.log("Hasil Pengurangan 20 - 5 =", kurang)
+// Output yang akan keluar adalah 15
+
+// PERKALIAN
+const perkalian = a * b
+console.log("Hasil Perkalian 20 x 5 =", perkalian)
+// Output yang akan keluar adalah 100
+
+// PEMBAGIAN
+const pembagian = a / b 
+console.log("Hasil Pembagian 20 : 5 =", pembagian)
+// Output yang akan keluar adalah 4
+
+// PERSENAN
+const persen = a % b
+console.log("Hasil Persenan 20%/5% =", persen)
+// Output yang akan keluar adalah 0
